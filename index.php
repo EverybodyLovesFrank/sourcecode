a250e5d54201fb66172d50fc9ad3e38fe08eb5e5
Frank Pernicano <git@keyboarddevil.com>
2011-08-27 17:27:37 -0400

    Fixed XHTML errors

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ else if (isset($_GET["formErrors"])) {
 		<h3>Other Sites I've Built</h3>
 		<div id="OtherBox">
 			<div class="boxgrid captionfull">
-				<img class="img" src="img/other/pc.jpg"/>
+				<img class="img" src="img/other/pc.jpg" alt="Princess Charming" />
 				<div class="cover boxcaption">
 					<h4>Princess Charming</h4>
 					<p>Site uses XHTML, CSS, PHP, Prototype JS, Flickr API, and the Facebook Connect API.</p>
@@ -98,7 +98,7 @@ else if (isset($_GET["formErrors"])) {
 			</div>
 
 			<div class="boxgrid captionfull">
-				<img class="img" src="img/other/md.jpg"/>
+				<img class="img" src="img/other/md.jpg" alt="Mobile Designs" />
 				<div class="cover boxcaption">
 					<h4>Mobile Designs</h4>
 					<p>Site uses XHTML, CSS, PHP, and a custom "Gallery" install (gallery.sourceforge.net).</p>
@@ -107,7 +107,7 @@ else if (isset($_GET["formErrors"])) {
 			</div>
 
 			<div class="boxgrid captionfull">
-				<img class="img" src="img/other/vd.jpg"/>
+				<img class="img" src="img/other/vd.jpg" alt="Virtual DJ'z" />
 				<div class="cover boxcaption">
 					<h4>Virtual DJ'z Entertainment</h4>
 					<p>Site uses XHTML CSS, and JavaScript.</p>
@@ -116,16 +116,16 @@ else if (isset($_GET["formErrors"])) {
 			</div>
 
 			<div class="boxgrid captionfull">
-				<img class="img" src="img/other/ce.jpg"/>
+				<img class="img" src="img/other/ce.jpg" alt="Compact Evolution" />
 				<div class="cover boxcaption">
 					<h4>Compact Evolution</h4>
 					<p>Site uses XHTML, CSS, PHP, and MySQL.</p>
-					<p>Site offline <a href="/compactevo/">copy saved here</a> (some links may not work).
+					<p>Site offline <a href="/compactevo/">copy saved here</a> (some links may not work).</p>
 				</div>		
 			</div>
 
 			<div class="boxgrid captionfull">
-				<img class="img" src="img/other/sp.jpg"/>
+				<img class="img" src="img/other/sp.jpg" alt="SPC" />
 				<div class="cover boxcaption">
 					<h4>St Petersburg College</h4>
 					<p>4 month contract to completely redesign and re-skin. XHTML, CSS, JavaScript, and Perl.</p>
@@ -134,7 +134,7 @@ else if (isset($_GET["formErrors"])) {
 			</div>
 
 			<div class="boxgrid captionfull">
-				<img class="img" src="img/other/is.jpg"/>
+				<img class="img" src="img/other/is.jpg" alt="ISS" />
 				<div class="cover boxcaption">
 					<h4>Industrial Sound Studios</h4>
 					<p>Recording studio based in California. Site uses XHTML, CSS, Flickr API, and JavaScript.</p>
@@ -143,7 +143,7 @@ else if (isset($_GET["formErrors"])) {
 			</div>
 
 			<div class="boxgrid captionfull">
-				<img class="img" src="img/other/el.jpg"/>
+				<img class="img" src="img/other/el.jpg" alt="Everybody Loves Frank" />
 				<div class="cover boxcaption">
 					<h4>EverybodyLovesFrank</h4>
 					<p>This site!  XHTML, CSS, PHP, and jQuery JavaScript</p>
@@ -165,28 +165,28 @@ else if (isset($_GET["formErrors"])) {
 			<p>"You can learn a lot about a person's technical ability by their attitude toward poor craftsmanship. During my time as an IT consultant at Walt Disney Parks and Resorts Online I had the opportunity to work adjacent to Frank for a significant period. While I've not had the chance to directly collaborate with him, the discussions we had regarding the issues facing our respective teams made me aware that Frank shares with me both a deeply internalized love of elegant systems, and an equally passionate aversion to shoddy engineering. When I interview prospective candidates for positions within Accenture, this is one of the first things that I look for. It implies a level personal engagement that you will not find otherwise. The deliverable I've seen of his have quickly validated this philosophy. I'd bring frank in to work on any technology with which he has familiarity without hesitation... He's also pretty funny."</p>
 		</div>
 		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=52566734&authType=name&authToken=52rt&trk=tyah">Tristan Rouse</a>, Senior Software Engineer, Accenture
+		<a href="http://www.linkedin.com/profile/view?id=52566734&amp;authType=name&amp;authToken=52rt&amp;trk=tyah">Tristan Rouse</a>, Senior Software Engineer, Accenture
 
 		<div class="bubbleTop"></div>
 		<div class="bubbleBody">
 			<p>"Frank was instrumental in our development and architecture of Disney's online ticketing and hotel booking systems. Together with Frank and the rest of the team, the rewrite of the booking system grew revenues over 100%, far exceeding all estimates and becoming a billion dollar product in the process. Frank is a very hard worker, versatile, capable and self motivated. If Frank is given a task, he picks it up and makes sure it's handled, even if its new ground for him. He was an integral part of the DisneyWorld.com team when I was there, and I wouldn't hesitate to recommend him to even my most trusted business contacts."</p>
 		</div>
 		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=8502008&authType=name&authToken=m-jX&trk=tyah">James Thomas</a>, Web Developer (Front end), Walt Disney Internet Group
+		<a href="http://www.linkedin.com/profile/view?id=8502008&amp;authType=name&amp;authToken=m-jX&amp;trk=tyah">James Thomas</a>, Web Developer (Front end), Walt Disney Internet Group
 
 		<div class="bubbleTop"></div>
 		<div class="bubbleBody">
 			<p>"Frank is all the personalities you want in a developer rolled into one: philosopher, idealist, engineer, architect, comedian, ninja. In baseball parlance, Frank is valuable in the field and in the clubhouse. Whether you need a captain or a teammate, you need Frank. To put it simply, Frank is anti-bad-stuff and pro-good-stuff, and that is what you look for in a web developer. I'd work with him repeatedly."</p>
 		</div>
 		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=11658451&authType=name&authToken=Ls1N">Matt Alexander</a>, Web Architect, Walt Disney Parks and Resorts Online
+		<a href="http://www.linkedin.com/profile/view?id=11658451&amp;authType=name&amp;authToken=Ls1N">Matt Alexander</a>, Web Architect, Walt Disney Parks and Resorts Online
 
 		<div class="bubbleTop"></div>
 		<div class="bubbleBody">
 			<p>"Frank is an extremely talented developer, skilled at what he does, communicative, open to other points of view, and works as well in a team as he does as an independent contributor. I've worked with Frank on and off for over 5 years at this writing, in the same team and in different teams, and few developers have impressed me as much as Frank has. My team, as a Sustainment/Operations team, is fortunate to have him back after he was gone for some time in the Projects space, and I myself am very glad to be working with a developer of his caliber once again."</p>
 		</div>
 		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=7188031&authType=name&authToken=H9yJ&trk=tyah">Rob Lune</a>, Senior Developer, Walt Disney Parks and Resorts Online
+		<a href="http://www.linkedin.com/profile/view?id=7188031&amp;authType=name&amp;authToken=H9yJ&amp;trk=tyah">Rob Lune</a>, Senior Developer, Walt Disney Parks and Resorts Online
 
 		<p class="accoladesBy">Quotes taken from my <a href="http://www.linkedin.com/in/fpernicano">LinkedIn profile</a></p>
 	</div>
