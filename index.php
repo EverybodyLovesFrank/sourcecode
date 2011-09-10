5f047fa91d269a07965017444968ea19d7912d42
Frank Pernicano <git@keyboarddevil.com>
2011-09-10 15:37:13 -0400

    Removed rounded corner img's, updated CSS with border-rad

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ else if (isset($_GET["formErrors"])) {
 </div>
 
 <div id="ModuleWork" class="moduleShowHide">
-	<div class="moduleTop"></div>
+	<!-- <div class="moduleTop"></div> -->
 	<div class="moduleBody">
 		<h3>Work history</h3>
 		<img class="workImage" src="img/disneyLogo.png" alt="Disney" />
@@ -64,27 +64,23 @@ else if (isset($_GET["formErrors"])) {
 		Celebration,FL</span></p>
 		<p class="historyBody">Responsibilities included technical design, development and maintenance of Disney websites using Tea (an open-source JSP-like language), Zend PHP, JavaScript, CSS, and XHTML. Server administration in multiple environments (local, development, QA, load/stress, and production). Design and maintenance of complex content objects in Disney's propietary content management system. Management of numerous small development teams (1-3 direct reports). Project management: Wireframe/comp reviews, functional specifications, code standards, documentation, and development/staging/build procedures to align with PCI Compliance. Took on the roll of project integration lead for site projects moving into the sustainment space. Strong experience in Blueprint CSS, YUI, Prototype, and JQuery libraries.</p>
 		<hr />
-	</div>
 
-	<div class="moduleBody">
 		<img class="workImage" src="img/aaaLogo.png" alt="AAA" />
 		<p><span class="title">AAA National Office<br />
 		Heathrow,FL</span></p>
 		<p class="historyBody">Responsibilities included creating, modifying, and developing Vignette Content Aggregation Server agents for providing AAA.com with updated vendor content. Responsible for over 25 vendors including Carnival Cruise Lines, Disneyland, Disney World, and Disney Cruise Lines. Extensive experience with MS SQL, JavaScript, HTML, CSS, Vignette Content Aggregation Server, user interface design, and small online application development.</p>
 		<hr />
-	</div>
 
-	<div class="moduleBody">
 		<img class="workImage" src="img/chaseLogo.gif" alt="Chase" />
 		<p><span class="title">JP Morgan Chase Manhattan Bank<br />
 		Tampa,FL</span></p>
 		<p class="historyBody">Responsibilities included creating, modifying, and developing pages for both card acquisitions and card services. These included interactive forms and dynamic pages written HTML, CSS, JavaScript, and JSP for the front end. Duties included front-end design and development as well as development of the transaction layer written using Enterprise Java Beans.</p>
 	</div>
-	<div class="moduleBottom"></div>
+	<!-- <div class="moduleBottom"></div> -->
 </div>
  
 <div id="ModuleOther" class="moduleShowHide">
-	<div class="moduleTop"></div>
+	<!-- <div class="moduleTop"></div> -->
 	<div class="moduleBody">
 		<h3>Other Sites I've Built</h3>
 		<div id="OtherBox">
@@ -152,94 +148,100 @@ else if (isset($_GET["formErrors"])) {
 			</div>
 		</div>
 	</div>
-	<div class="moduleBottom"></div>
+	<!-- <div class="moduleBottom"></div> -->
 </div>
  
-<div id="ModuleAcc" class="moduleShowHide">
-	<div class="moduleTop"></div>
-	<div class="moduleBody">
-		<h3>Things my co-workers have said</h3>
+<div class="moduleParent">
+	<div id="ModuleAcc" class="moduleShowHide">
+		<!-- <div class="moduleTop"></div> -->
+		<div class="moduleBody">
+			<h3>Things my co-workers have said</h3>
 
-		<div class="bubbleTop"></div>
-		<div class="bubbleBody">
-			<p>"You can learn a lot about a person's technical ability by their attitude toward poor craftsmanship. During my time as an IT consultant at Walt Disney Parks and Resorts Online I had the opportunity to work adjacent to Frank for a significant period. While I've not had the chance to directly collaborate with him, the discussions we had regarding the issues facing our respective teams made me aware that Frank shares with me both a deeply internalized love of elegant systems, and an equally passionate aversion to shoddy engineering. When I interview prospective candidates for positions within Accenture, this is one of the first things that I look for. It implies a level personal engagement that you will not find otherwise. The deliverable I've seen of his have quickly validated this philosophy. I'd bring frank in to work on any technology with which he has familiarity without hesitation... He's also pretty funny."</p>
+			<div class="bubbleTop"></div>
+			<div class="bubbleBody">
+				<p>"You can learn a lot about a person's technical ability by their attitude toward poor craftsmanship. During my time as an IT consultant at Walt Disney Parks and Resorts Online I had the opportunity to work adjacent to Frank for a significant period. While I've not had the chance to directly collaborate with him, the discussions we had regarding the issues facing our respective teams made me aware that Frank shares with me both a deeply internalized love of elegant systems, and an equally passionate aversion to shoddy engineering. When I interview prospective candidates for positions within Accenture, this is one of the first things that I look for. It implies a level personal engagement that you will not find otherwise. The deliverable I've seen of his have quickly validated this philosophy. I'd bring frank in to work on any technology with which he has familiarity without hesitation... He's also pretty funny."</p>
+			</div>
+			<div class="bubbleBottom"></div>
+			<a href="http://www.linkedin.com/profile/view?id=52566734&amp;authType=name&amp;authToken=52rt&amp;trk=tyah">Tristan Rouse</a>, Senior Software Engineer, Accenture
+
+			<div class="bubbleTop"></div>
+			<div class="bubbleBody">
+				<p>"Frank was instrumental in our development and architecture of Disney's online ticketing and hotel booking systems. Together with Frank and the rest of the team, the rewrite of the booking system grew revenues over 100%, far exceeding all estimates and becoming a billion dollar product in the process. Frank is a very hard worker, versatile, capable and self motivated. If Frank is given a task, he picks it up and makes sure it's handled, even if its new ground for him. He was an integral part of the DisneyWorld.com team when I was there, and I wouldn't hesitate to recommend him to even my most trusted business contacts."</p>
+			</div>
+			<div class="bubbleBottom"></div>
+			<a href="http://www.linkedin.com/profile/view?id=8502008&amp;authType=name&amp;authToken=m-jX&amp;trk=tyah">James Thomas</a>, Web Developer (Front end), Walt Disney Internet Group
+
+			<div class="bubbleTop"></div>
+			<div class="bubbleBody">
+				<p>"Frank is all the personalities you want in a developer rolled into one: philosopher, idealist, engineer, architect, comedian, ninja. In baseball parlance, Frank is valuable in the field and in the clubhouse. Whether you need a captain or a teammate, you need Frank. To put it simply, Frank is anti-bad-stuff and pro-good-stuff, and that is what you look for in a web developer. I'd work with him repeatedly."</p>
+			</div>
+			<div class="bubbleBottom"></div>
+			<a href="http://www.linkedin.com/profile/view?id=11658451&amp;authType=name&amp;authToken=Ls1N">Matt Alexander</a>, Web Architect, Walt Disney Parks and Resorts Online
+
+			<div class="bubbleTop"></div>
+			<div class="bubbleBody">
+				<p>"Frank is an extremely talented developer, skilled at what he does, communicative, open to other points of view, and works as well in a team as he does as an independent contributor. I've worked with Frank on and off for over 5 years at this writing, in the same team and in different teams, and few developers have impressed me as much as Frank has. My team, as a Sustainment/Operations team, is fortunate to have him back after he was gone for some time in the Projects space, and I myself am very glad to be working with a developer of his caliber once again."</p>
+			</div>
+			<div class="bubbleBottom"></div>
+			<a href="http://www.linkedin.com/profile/view?id=7188031&amp;authType=name&amp;authToken=H9yJ&amp;trk=tyah">Rob Lune</a>, Senior Developer, Walt Disney Parks and Resorts Online
+
+			<p class="accoladesBy">Quotes taken from my <a href="http://www.linkedin.com/in/fpernicano">LinkedIn profile</a></p>
 		</div>
-		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=52566734&amp;authType=name&amp;authToken=52rt&amp;trk=tyah">Tristan Rouse</a>, Senior Software Engineer, Accenture
-
-		<div class="bubbleTop"></div>
-		<div class="bubbleBody">
-			<p>"Frank was instrumental in our development and architecture of Disney's online ticketing and hotel booking systems. Together with Frank and the rest of the team, the rewrite of the booking system grew revenues over 100%, far exceeding all estimates and becoming a billion dollar product in the process. Frank is a very hard worker, versatile, capable and self motivated. If Frank is given a task, he picks it up and makes sure it's handled, even if its new ground for him. He was an integral part of the DisneyWorld.com team when I was there, and I wouldn't hesitate to recommend him to even my most trusted business contacts."</p>
-		</div>
-		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=8502008&amp;authType=name&amp;authToken=m-jX&amp;trk=tyah">James Thomas</a>, Web Developer (Front end), Walt Disney Internet Group
-
-		<div class="bubbleTop"></div>
-		<div class="bubbleBody">
-			<p>"Frank is all the personalities you want in a developer rolled into one: philosopher, idealist, engineer, architect, comedian, ninja. In baseball parlance, Frank is valuable in the field and in the clubhouse. Whether you need a captain or a teammate, you need Frank. To put it simply, Frank is anti-bad-stuff and pro-good-stuff, and that is what you look for in a web developer. I'd work with him repeatedly."</p>
-		</div>
-		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=11658451&amp;authType=name&amp;authToken=Ls1N">Matt Alexander</a>, Web Architect, Walt Disney Parks and Resorts Online
-
-		<div class="bubbleTop"></div>
-		<div class="bubbleBody">
-			<p>"Frank is an extremely talented developer, skilled at what he does, communicative, open to other points of view, and works as well in a team as he does as an independent contributor. I've worked with Frank on and off for over 5 years at this writing, in the same team and in different teams, and few developers have impressed me as much as Frank has. My team, as a Sustainment/Operations team, is fortunate to have him back after he was gone for some time in the Projects space, and I myself am very glad to be working with a developer of his caliber once again."</p>
-		</div>
-		<div class="bubbleBottom"></div>
-		<a href="http://www.linkedin.com/profile/view?id=7188031&amp;authType=name&amp;authToken=H9yJ&amp;trk=tyah">Rob Lune</a>, Senior Developer, Walt Disney Parks and Resorts Online
-
-		<p class="accoladesBy">Quotes taken from my <a href="http://www.linkedin.com/in/fpernicano">LinkedIn profile</a></p>
+		<!-- <div class="moduleBottom"></div> -->
 	</div>
-	<div class="moduleBottom"></div>
 </div>
 
-<div id="ModuleContact" class="moduleShowHide">
-	<div class="moduleTop"></div>
-	<div class="moduleBody">
-		<form id="ContactForm" method="post" action="formMail.php">
-			<h3>Contact me:</h3>
-			<table>
-			<tr>
-				<td><label for="name">Your name:</label></td>
-				<td><input type="text" name="name" id="name" class="required" /></td>
-			</tr>
-			<tr>
-				<td><label for="email">Your email:</label></td>
-				<td><input type="text" name="email" id="email" class="required email" /></td>
-			</tr>
-			<tr>
-				<td><label for="phone">Your phone:</label></td>
-				<td><input type="text" id="phone" name="phone" /></td>
-			</tr>
-			<tr>
-				<td class="topAlign"><label for="message">Message:</label></td>
-				<td><textarea rows="2" cols="30" id="message" name="message" class="required"></textarea></td>
-			</tr>
-			<tr>
-				<td colspan="2"><input class="submit" type="submit" name="submit" value="Send it" /></td>
-			</tr>
-			</table>
-		</form>
-		<div id="ContactSites">
-			<h3>Frank around the web:</h3>
-			<div id="ContactIcons">
-				<a href="https://github.com/EverybodyLovesFrank"><span id="GitBtn">GitHub</span></a>
-				<a href="https://www.facebook.com/frank.pernicano"><span id="FaceBtn">Facebook</span></a>
-				<a href="http://meetups.jquery.com/group/centralflorida"><span id="JqueryBtn">JQuery Meetups</span></a>
-				<a href="http://www.linkedin.com/in/fpernicano"><span id="LinkedBtn">Linked In</span></a>
-				<a href="https://plus.google.com/117309864495177542659"><span id="GoogBtn">Google +</span></a>
+<div class="moduleParent">
+	<div id="ModuleContact" class="moduleShowHide">
+		<!-- <div class="moduleTop"></div> -->
+		<div class="moduleBody">
+			<form id="ContactForm" method="post" action="formMail.php">
+				<h3>Contact me:</h3>
+				<table>
+				<tr>
+					<td><label for="name">Your name:</label></td>
+					<td><input type="text" name="name" id="name" class="required" /></td>
+				</tr>
+				<tr>
+					<td><label for="email">Your email:</label></td>
+					<td><input type="text" name="email" id="email" class="required email" /></td>
+				</tr>
+				<tr>
+					<td><label for="phone">Your phone:</label></td>
+					<td><input type="text" id="phone" name="phone" /></td>
+				</tr>
+				<tr>
+					<td class="topAlign"><label for="message">Message:</label></td>
+					<td><textarea rows="2" cols="30" id="message" name="message" class="required"></textarea></td>
+				</tr>
+				<tr>
+					<td colspan="2"><input class="submit" type="submit" name="submit" value="Send it" /></td>
+				</tr>
+				</table>
+			</form>
+			<div id="ContactSites">
+				<h3>Frank around the web:</h3>
+				<div id="ContactIcons">
+					<a href="https://github.com/EverybodyLovesFrank"><span id="GitBtn">GitHub</span></a>
+					<a href="https://www.facebook.com/frank.pernicano"><span id="FaceBtn">Facebook</span></a>
+					<a href="http://meetups.jquery.com/group/centralflorida"><span id="JqueryBtn">JQuery Meetups</span></a>
+					<a href="http://www.linkedin.com/in/fpernicano"><span id="LinkedBtn">Linked In</span></a>
+					<a href="https://plus.google.com/117309864495177542659"><span id="GoogBtn">Google +</span></a>
+				</div>
 			</div>
 		</div>
+		<!-- <div class="moduleBottom"></div> -->
 	</div>
-	<div class="moduleBottom"></div>
 </div>
 
-<div id="ModuleFooter">
-	<div class="moduleTop"></div>
-	<div class="moduleBody">
-		&copy; <?php echo date('Y'); ?> Frank T Pernicano | frank@everybodylovesfrank.com
+<div class="moduleParent">
+	<div id="ModuleFooter">
+		<!-- <div class="moduleTop"></div> -->
+		<div class="moduleBody">
+			&copy; <?php echo date('Y'); ?> Frank T Pernicano | frank@everybodylovesfrank.com
+		</div>
+		<!-- <div class="moduleBottom"></div> -->
 	</div>
-	<div class="moduleBottom"></div>
 </div>
 
 <script type="text/javascript" src="js/jquery.js"></script>
